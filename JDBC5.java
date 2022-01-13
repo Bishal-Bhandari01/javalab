06e4da79c4ff4fa5543fda4726aed4b80e6febab
BISHAL BHANDARI <80243759+James-smith2@users.noreply.github.com>
2022-01-13 16:12:13 +0545

    main

---
+++ JDBC5.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+
+class JDBC5 {
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String url = "jdbc:mysql://localhost:3306/bca_third_2020?serverTimezone=UTC";
+			String un = "root";
+			String pw = "";
+			Connection cn = DriverManager.getConnection(url, un, pw);
+			deleteAll(cn);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	private static void deleteAll(Connection cn) throws SQLException {
+		Statement stat = cn.createStatement();
+
+		String query = "delete from student";
+		stat.executeUpdate(query);
+
+		System.out.println("All Record deleted!");
+	}
+}
---
+++ JDBC6.java
@@ -0,0 +1,30 @@
+import java.sql.*;
+
+class JDBC6 {
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String url = "jdbc:mysql://localhost:3306/bca_third_2020?serverTimezone=UTC";
+			String un = "root";
+			String pw = "";
+			Connection cn = DriverManager.getConnection(url, un, pw);
+			display(cn);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	private static void display(Connection cn) throws SQLException {
+		Statement stat = cn.createStatement();
+
+		String query = "select * from student";
+		System.out.println("Reading records...");
+		ResultSet rs = stat.executeQuery(query);
+		while (rs.next()) {
+			System.out.print("Roll NO: " + rs.getString("roll_no"));
+			System.out.print(" Name: " + rs.getString("name"));
+			System.out.print(" Course: " + rs.getString("course"));
+			System.out.println("\n");
+		}
+	}
+}
